44fb27cad7db9df152b743243792d0ab89d28da6
elamoreslomasgrande <elamoreslomasgrande>
2017-11-17 17:24:16 +0100

    Fix google maps api now it's working add functions to get distance

---
+++ app/src/main/java/com/example/elamoreslomasgrande/volley1/MapsActivity.java
@@ -0,0 +1,105 @@
+package com.example.elamoreslomasgrande.volley1;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    /*    public void aBuscar(View v){
+        hola = (EditText) findViewById(R.id.dios);
+        Log.d("traza",hola.getText().toString());
+        getLocationFromAddress(hola.getText().toString());
+    }
+    public void getLocationFromAddress(String strAddress)
+    {
+        //Create coder with Activity context - this
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+
+        try {
+            //Get latLng from String
+            address = coder.getFromLocationName(strAddress,5);
+
+            //check for null
+            if (address == null) {
+                return;
+            }
+
+            //Lets take first possibility from the all possibilities.
+            Address location=address.get(0);
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            //Put marker on map on that LatLng
+            Marker srchMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Destination").draggable(true));
+
+            //Animate and Zoon on that map location
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+            LatLng sydney = new LatLng(41.3863900,2.1687300 );
+            hola = (EditText) findViewById(R.id.dios);
+            hola.setText(getDistance(latLng,sydney));
+
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public String getDistance(LatLng my_latlong, LatLng frnd_latlong) {
+        Location l1 = new Location("One");
+        l1.setLatitude(my_latlong.latitude);
+        l1.setLongitude(my_latlong.longitude);
+
+        Location l2 = new Location("Two");
+        l2.setLatitude(frnd_latlong.latitude);
+        l2.setLongitude(frnd_latlong.longitude);
+
+        float distance = l1.distanceTo(l2);
+        String dist = distance + " M";
+        Log.d("traza",dist);
+        if (distance > 1000.0f) {
+            distance = distance / 1000.0f;
+            dist = distance + " KM";
+        }
+        return dist;
+    }*/
+}
