210fa1f3fbc58c15312f1661ea45759377e2b8a9
elamoreslomasgrande <elamoreslomasgrande>
2017-11-20 13:46:28 +0100

    Assegurar pagina Bifurcacion

---
+++ app/src/main/java/com/example/elamoreslomasgrande/volley1/Login/RecuperarPass.java
@@ -0,0 +1,15 @@
+package com.example.elamoreslomasgrande.volley1.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.elamoreslomasgrande.volley1.R;
+
+public class RecuperarPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_pass);
+    }
+}
