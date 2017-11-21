865439b5ced24d8081ec8e214a8e78aa4ac99993
Selfnir <ismaelcm8@gmail.com>
2017-11-21 12:55:22 +0100

    Subir los 33 archivos

---
+++ app/src/main/java/com/example/elamoreslomasgrande/volley1/NavigationChildren/CentroAyuda.java
@@ -0,0 +1,32 @@
+package com.example.elamoreslomasgrande.volley1.NavigationChildren;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.elamoreslomasgrande.volley1.R;
+
+public class CentroAyuda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_centro_ayuda);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
