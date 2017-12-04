6eb3efd52dfb9e5e96ebe3c12b20ec7996a86118
Selfnir <ismaelcm8@gmail.com>
2017-12-04 18:14:42 +0100

    Upgraded detalles oferta, al estar cerrado y la imagen

--- app/src/main/java/com/example/elamoreslomasgrande/volley1/Ofertas/detallesOferta.java
+++ app/src/main/java/com/example/elamoreslomasgrande/volley1/Ofertas/detallesOferta.java
@@ -1,25 +1,30 @@
 package com.example.elamoreslomasgrande.volley1.Ofertas;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.elamoreslomasgrande.volley1.Contador;
 import com.example.elamoreslomasgrande.volley1.R;
+import com.squareup.picasso.Picasso;
 
 public class detallesOferta extends AppCompatActivity {
     private TextView titulo, descripcion, remunerado, jornada, inscritos, restTime;
-    private Button estado;
-
+    private Button estado, inscribirse;
+    private ImageView imagenOferta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles__oferta);
         Oferta a = (Oferta) getIntent().getExtras().getSerializable("oferta");
 
+
+        imagenOferta = findViewById(R.id.foto);
         titulo = findViewById(R.id.titulo);
         descripcion = findViewById(R.id.lugar);
         remunerado = findViewById(R.id.remunerado);
@@ -27,6 +32,7 @@ public class detallesOferta extends AppCompatActivity {
         inscritos = findViewById(R.id.inscritos);
         restTime = findViewById(R.id.restTime);
         estado = findViewById(R.id.ofertaEstadoButton);
+        inscribirse = findViewById(R.id.Inscribirse);
 
 
         Toast.makeText(this, a.toString(), Toast.LENGTH_SHORT).show();
@@ -36,7 +42,14 @@ public class detallesOferta extends AppCompatActivity {
         remunerado.setText(a.getRenumerado());
         jornada.setText("Jornada "+a.getJornada());
 
+
+
+        Picasso.with(this).load("http://46.105.28.25:3020/images/"+ a.getFotoportada()).into(imagenOferta);
         Contador.countDown(a.getFechalimite(),1000, restTime, estado);
+        if (estado.getText().equals("Cerrado")){
+            inscribirse.setText("Finalizado");
+            inscribirse.setClickable(false);
+        }
 
 
     }
